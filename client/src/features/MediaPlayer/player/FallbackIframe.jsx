46ab761fa6aac4b075bcb6731904058a385bbe91
Ray <77224234+rayhan5497@users.noreply.github.com>
2026-01-08 19:55:50 +0600

    fix: remove sandbox in player/fallbackIframe

--- client/src/features/MediaPlayer/player/FallbackIframe.jsx
+++ client/src/features/MediaPlayer/player/FallbackIframe.jsx
@@ -6,7 +6,6 @@ const CloudflareChallenge = ({ url }) => {
       key={url}
       className="absolute top-0 left-0 w-full h-full border-0 bg-black rounded"
       allowFullScreen
-      sandbox="allow-same-origin allow-scripts allow-forms allow-popups"
     />
   );
 };
